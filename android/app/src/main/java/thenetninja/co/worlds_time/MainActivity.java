9b4f1069f854330682a014b0155368fb29105b4e
SyazwanAziz <57909182+SyazwanAziz@users.noreply.github.com>
2020-11-22 00:42:12 +0800

    first commit

---
+++ android/app/src/main/java/thenetninja/co/worlds_time/MainActivity.java
@@ -0,0 +1,6 @@
+package thenetninja.co.worlds_time;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/services/world_time.dart
@@ -0,0 +1,46 @@
+import 'package:http/http.dart';
+import 'dart:convert';
+import 'package:intl/intl.dart';
+
+class WorldTime {
+  String location; //location name for the ui
+  String time; //time in that  location
+  String flag; //url to an asset flag icon
+  String url; //location url for Api endpoint
+  bool isDayTime; //true or false if daytime or not
+
+  WorldTime({this.location, this.flag, this.url});
+
+  Future<void> getTime() async {
+
+    try{
+      //make the request
+      Response response = await get('https://worldtimeapi.org/api/timezone/$url');
+      Map data = jsonDecode(response.body);
+      //print(data);
+
+      //get properties from data
+      String datetime = data['datetime'];
+      String offset = data['utc_offset'].substring(1, 3);
+      //print(datetime);
+      //print(offset);
+
+      //create datetime object
+      DateTime now = DateTime.parse(datetime);
+      now = now.add(Duration(hours: int.parse(offset)));
+
+      print(now);
+
+      //set the time property
+      isDayTime = now.hour > 6 && now.hour < 19 ? true : false;
+      time = DateFormat.jm().format(now);
+    }
+    catch(e){
+      print('caught error: $e');
+      time = 'could not get time data';
+    }
+
+  }
+}
+
+// WorldTime instance = WorldTime(location: 'Kuala Lumpur', flag: 'malaysia.png', url: 'Asia/Kuala_Lumpur');
